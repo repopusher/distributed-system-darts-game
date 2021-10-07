4f7ca48eb2f4cf7f9b92cf4af4c7a054d1b55634
Tobias Lennon <tobias.lennon@mycit.ie>
2021-10-07 21:03:45 +0100

    commiting project files

--- test.py
+++ test.py
@@ -6,3 +6,9 @@ dao = darts_match_dao.DartsMatchDao()
 match = darts_match.DartsMatch('501', 'Dupe', 'Dup1')
 
 dao.add(match)
+
+dao2 = darts_match_dao.DartsMatchDao()
+
+match = darts_match.DartsMatch('501', 'Dupe2', 'Dup2')
+
+dao2.add(match)
